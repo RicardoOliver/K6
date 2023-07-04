766d637c72efa7c7ab1e8380f8550d094722f0e0
Ricardo <r.c.d.1985@hotmail.com>
2023-07-04 08:59:57 -0300

    Update get-load-test.js

--- K6_test/get-load-test.js
+++ K6_test/get-load-test.js
@@ -1,7 +1,3 @@
-//Realizando Requisições HTTP
-//cd modulo3
-//comando executar teste : K6 run aula2.js
-
 import http from 'k6/http';
 import {check } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
